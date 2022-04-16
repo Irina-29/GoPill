f4ce399aca9dccab95e559f356d00321102c44a5
Irina-29 <irina.barbu229@gmail.com>
2022-04-17 00:16:03 +0300

    update List

--- app/screens/ItemList.tsx
+++ app/screens/ItemList.tsx
@@ -1,73 +1,76 @@
 import React, { useEffect, useState } from "react";
 import MainLayout from "./Layout";
-import { Button, Card, DataTable, Divider, Headline, List, Paragraph, Subheading, Title } from "react-native-paper";
-import { View } from "react-native";
+import { Button, Card, DataTable, Divider, Headline, List, Paragraph, Subheading, Title, FAB } from "react-native-paper";
+import { View, ScrollView } from "react-native";
 import { white } from "react-native-paper/lib/typescript/styles/colors";
 
 const ItemListScreen = ({ navigation }: any) => {
 
-        const [data, setData] = useState<{ id?: string, label: string, quantity: number }[]>([]);
-        /**
-         https://callstack.github.io/react-native-paper/data-table.html
-         */
-        return <MainLayout>
-                <View style={{ alignSelf: 'stretch', display: 'flex', flexDirection: 'column' }}>
-                        <Headline style={{textAlign: "center", lineHeight: 100, color: 'white', backgroundColor: '#64b5f6' }}>My List </Headline >
-                        <View>
-                                {/* LIST -> SWIPE*/}
-                                <List.Section>
-                                        <List.Item
-                                                title="Panadol"
-                                                description="Only 3x pills left"
-                                                left={props => <List.Icon {...props} icon="pill" />}
-                                        />
-                                        <Divider />
-                                        <List.Item
-                                                title="Paracetamol"
-                                                description="Only 12x pills left"
-                                                left={props => <List.Icon {...props} icon="pill" />}
-                                        />
-                                        <Divider />
-                                        <List.Item
-                                                title="Vitamin C"
-                                                description="Only 27x pills left"
-                                                left={props => <List.Icon {...props} icon="pill" />}
-                                        />
-                                        <Divider />
-                                        <List.Item
-                                                title="Vitamin D3"
-                                                description="Only 28x pills left"
-                                                left={props => <List.Icon {...props} icon="pill" />}
-                                        />
-                                        <Divider />
-                                        <List.Item
-                                                title="Nurofen"
-                                                description="Only 39x pills left"
-                                                left={props => <List.Icon {...props} icon="pill" />}
-                                        />
-                                        <Divider />
-                                        <List.Item
-                                                title="Aspirin"
-                                                description="Only 50x pills left"
-                                                left={props => <List.Icon {...props} icon="pill" />}
-                                        />
-                                        <Divider />
-                                        <List.Item
-                                                title="Calcivid"
-                                                description="Only 100x pills left"
-                                                left={props => <List.Icon {...props} icon="pill" />}
-                                        />
-                                        <Divider />
-                                        <List.Item
-                                                title="Calcivid"
-                                                description="Only 100x pills left"
-                                                left={props => <List.Icon {...props} icon="pill" />}
-                                        />
-                                        <Divider />
-                                </List.Section>
-                        </View>
+    const [data, setData] = useState<{ id?: string, label: string, quantity: number }[]>([]);
+    /**
+     https://callstack.github.io/react-native-paper/data-table.html
+        */
+    return (
+        <MainLayout>
+            <View style={{ alignSelf: 'stretch', display: 'flex', flexDirection: 'column' }}>
+                <Headline style={{textAlign: "center", lineHeight: 80, color: 'white', backgroundColor: '#64b5f6' }}>My List</Headline >
+                <View style={{marginLeft: '5%', marginRight: '5%'}}>
+                <FAB
+                    style={{margin: 10, right: 0, bottom: 0, backgroundColor: "#64b5f6", position: 'absolute'}}
+                    // label={"ADD"}
+                    icon="plus"
+                    color="white"
+                    onPress={() => console.log('Pressed')}
+                />
+                    <ScrollView>
+                        <List.Section>
+                            <List.Item
+                                    title="Panadol"
+                                    description="3x pills"
+                                    left={props => <List.Icon {...props} icon="pill" />}
+                            />
+                            <Divider />
+                            <List.Item
+                                    title="Paracetamol"
+                                    description="12x pills"
+                                    left={props => <List.Icon {...props} icon="pill" />}
+                            />
+                            <Divider />
+                            <List.Item
+                                    title="Vitamin C"
+                                    description="27x pills"
+                                    left={props => <List.Icon {...props} icon="pill" />}
+                            />
+                            <Divider />
+                            <List.Item
+                                    title="Vitamin D3"
+                                    description="28x pills"
+                                    left={props => <List.Icon {...props} icon="pill" />}
+                            />
+                            <Divider />
+                            <List.Item
+                                    title="Nurofen"
+                                    description="39x pills"
+                                    left={props => <List.Icon {...props} icon="pill" />}
+                            />
+                            <Divider />
+                            <List.Item
+                                    title="Aspirin"
+                                    description="50x pills"
+                                    left={props => <List.Icon {...props} icon="pill" />}
+                            />
+                            <Divider />
+                            <List.Item
+                                    title="Calcivid"
+                                    description="100x pills"
+                                    left={props => <List.Icon {...props} icon="pill" />}
+                            />
+                        </List.Section>
+                    </ScrollView>
                 </View>
+            </View>
         </MainLayout >
+    );
 }
 
 export default ItemListScreen;
