a656c66ca02f2468e2263c3a1164ee27ee6e1830
Irina-29 <irina.barbu229@gmail.com>
2022-04-17 14:59:15 +0300

    update

--- app/screens/ItemList.tsx
+++ app/screens/ItemList.tsx
@@ -1,75 +1,123 @@
 import React, { useEffect, useState } from "react";
 import MainLayout from "./Layout";
-import { Button, Card, DataTable, Divider, Headline, List, Paragraph, Subheading, Title, FAB } from "react-native-paper";
-import { View, ScrollView } from "react-native";
-import { white } from "react-native-paper/lib/typescript/styles/colors";
+import { Divider, Headline, List, FAB, Modal, Portal, Provider, Button, Paragraph} from "react-native-paper";
+import { View, ScrollView, TouchableOpacity } from "react-native";
 
 const ItemListScreen = ({ navigation }: any) => {
 
-    const [data, setData] = useState<{ id?: string, label: string, quantity: number }[]>([]);
-    /**
-     https://callstack.github.io/react-native-paper/data-table.html
-        */
+    const [visible, setVisible] = React.useState(false);
+
+    const showModal = () => setVisible(true);
+    const hideModal = () => setVisible(false);
+    const containerStyle = {backgroundColor: 'white', padding: 20};
+
     return (
+        <><Portal>
+            <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
+                <Paragraph>Example Modal.  Click outside this area to dismiss.</Paragraph>
+            </Modal>
+        </Portal>
         <MainLayout>
-            <View style={{ alignSelf: 'stretch', display: 'flex', flexDirection: 'column' }}>
-                <Headline style={{textAlign: "center", lineHeight: 80, color: 'white', backgroundColor: '#64b5f6' }}>My List</Headline >
-                <View style={{marginLeft: '5%', marginRight: '5%'}}>
-                <FAB
-                    style={{margin: 10, right: 0, bottom: 0, backgroundColor: "#64b5f6", position: 'absolute'}}
-                    // label={"ADD"}
-                    icon="plus"
-                    color="white"
-                    onPress={() => console.log('Pressed')}
-                />
-                    <ScrollView>
-                        <List.Section>
-                            <List.Item
-                                    title="Panadol"
-                                    description="3x pills"
-                                    left={props => <List.Icon {...props} icon="pill" />}
-                            />
-                            <Divider />
-                            <List.Item
+                <View style={{ alignSelf: 'stretch', display: 'flex', flexDirection: 'column' }}>
+                    <Headline style={{ textAlign: "center", lineHeight: 80, color: 'white', backgroundColor: '#64b5f6' }}>My List</Headline>
+                    <View>
+                        <ScrollView>
+                            <List.Section style={{marginBottom: "12%"}}>
+                                {/* <TouchableOpacity onPress={showModal}>
+                                    <List.Item
+                                        title="Panadol"
+                                        description="3x pills"
+                                        left={props => <List.Icon {...props} icon="pill" />} />
+                                </TouchableOpacity>
+                                <Divider /> */}
+                                <TouchableOpacity onPress={showModal}>
+                                <List.Item
+                                    style={{marginLeft:"2%"}}
                                     title="Paracetamol"
                                     description="12x pills"
-                                    left={props => <List.Icon {...props} icon="pill" />}
-                            />
-                            <Divider />
-                            <List.Item
+                                    left={props => <List.Icon {...props} icon="pill" />} />
+                                </TouchableOpacity>
+                                <Divider />
+                                <TouchableOpacity onPress={showModal}>
+                                <List.Item
+                                    style={{marginLeft:"2%"}}
                                     title="Vitamin C"
                                     description="27x pills"
-                                    left={props => <List.Icon {...props} icon="pill" />}
-                            />
-                            <Divider />
-                            <List.Item
+                                    left={props => <List.Icon {...props} icon="pill" />} />
+                                </TouchableOpacity>
+                                <Divider />
+                                <TouchableOpacity onPress={showModal}>
+                                <List.Item
+                                    style={{marginLeft:"2%"}}
                                     title="Vitamin D3"
                                     description="28x pills"
-                                    left={props => <List.Icon {...props} icon="pill" />}
-                            />
-                            <Divider />
-                            <List.Item
+                                    left={props => <List.Icon {...props} icon="pill" />} />
+                                </TouchableOpacity>
+                                <Divider />
+                                <TouchableOpacity onPress={showModal}>
+                                <List.Item
+                                    style={{marginLeft:"2%"}}
                                     title="Nurofen"
                                     description="39x pills"
-                                    left={props => <List.Icon {...props} icon="pill" />}
-                            />
-                            <Divider />
-                            <List.Item
+                                    left={props => <List.Icon {...props} icon="pill" />} />
+                                </TouchableOpacity>
+                                <Divider />
+                                <TouchableOpacity onPress={showModal}>
+                                <List.Item
+                                    style={{marginLeft:"2%"}}
                                     title="Aspirin"
                                     description="50x pills"
-                                    left={props => <List.Icon {...props} icon="pill" />}
-                            />
-                            <Divider />
-                            <List.Item
+                                    left={props => <List.Icon {...props} icon="pill" />} />
+                                </TouchableOpacity>
+                                <Divider />
+                                <TouchableOpacity onPress={showModal}>
+                                <List.Item
+                                    style={{marginLeft:"2%"}}
                                     title="Calcivid"
                                     description="100x pills"
-                                    left={props => <List.Icon {...props} icon="pill" />}
-                            />
-                        </List.Section>
-                    </ScrollView>
+                                    left={props => <List.Icon {...props} icon="pill" />} />
+                                </TouchableOpacity>
+
+                                {/* <Divider />
+                                <TouchableOpacity onPress={showModal}>
+                                <List.Item
+                                    style={{marginLeft:"2%"}}
+                                    title="Vitamin D3"
+                                    description="28x pills"
+                                    left={props => <List.Icon {...props} icon="pill" />} />
+                                </TouchableOpacity>
+                                <Divider />
+                                <TouchableOpacity onPress={showModal}>
+                                <List.Item
+                                    style={{marginLeft:"2%"}}
+                                    title="Paracetamol"
+                                    description="12x pills"
+                                    left={props => <List.Icon {...props} icon="pill" />} />
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={showModal}>
+                                <List.Item
+                                    style={{marginLeft:"2%"}}
+                                    title="Nurofen"
+                                    description="39x pills"
+                                    left={props => <List.Icon {...props} icon="pill" />} />
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={showModal}>
+                                <List.Item
+                                    style={{marginLeft:"2%"}}
+                                    title="Vitamin D3"
+                                    description="28x pills"
+                                    left={props => <List.Icon {...props} icon="pill" />} />
+                                </TouchableOpacity> */}
+                            </List.Section>
+                        </ScrollView>
+                        <FAB
+                            style={{ width: "16%", top: 452, left: 272, backgroundColor: "#64b5f6", position: 'absolute' }}
+                            icon="plus"
+                            color="white"
+                            onPress={() => console.log('Pressed')} />
+                    </View>
                 </View>
-            </View>
-        </MainLayout >
+            </MainLayout></>
     );
 }
 
--- app/screens/ScheduleList.tsx
+++ app/screens/ScheduleList.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import MainLayout from "./Layout";
-import { Button, Card, DataTable, Divider, List, Paragraph, Subheading, Title } from "react-native-paper";
-import { View } from "react-native";
+import { Button, Card, DataTable, Divider, List, Paragraph, Subheading, Title, Headline } from "react-native-paper";
+import { ScrollView, View } from "react-native";
 
 const ScheduleListScreen = ({ navigation }: any) => {
 
@@ -10,49 +10,59 @@ const ScheduleListScreen = ({ navigation }: any) => {
      https://callstack.github.io/react-native-paper/data-table.html
      */
     return <MainLayout>
-        <View style={{ alignSelf: 'stretch', display: 'flex', flexDirection: 'column', marginTop: '5%', marginLeft: '5%', marginRight: '5%' }}>
-            <Subheading>Upcoming alarms</Subheading>
-            <View style={{ marginTop: '2%' }}>
+        <View style={{ alignSelf: 'stretch', display: 'flex', flexDirection: 'column' }}>
+        {/* <View style={{ alignSelf: 'stretch', display: 'flex', flexDirection: 'column', marginTop: '5%', marginLeft: '5%', marginRight: '5%' }}> */}
+        <Headline style={{ textAlign: "center", lineHeight: 80, color: 'white', backgroundColor: '#64b5f6' }}>My Schedule</Headline>
+            <View style={{ marginTop: '4%' }}>
+                <Subheading style={{marginLeft:"4%", marginBottom: "2%"}}>Upcoming alarms</Subheading>
                 {/* LIST -> SWIPE*/}
-                <List.Section>
+                <ScrollView>
+                <List.Section style={{marginBottom: "12%"}}>
                     <Divider />
                     <List.Item
+                        style={{marginLeft:"2%"}}
                         title="Take Paracetamol"
                         description="Today, 6:00 PM"
                         left={props => <List.Icon {...props} icon="pill" />}
                     />
                     <Divider />
                     <List.Item
+                        style={{marginLeft:"2%"}}
                         title="Take Vitamin C"
                         description="Today, 6:30 PM"
                         left={props => <List.Icon {...props} icon="pill" />}
                     />
                     <Divider />
                     <List.Item
+                        style={{marginLeft:"2%"}}
                         title="Take Vitamin D3"
                         description="Tomorrow, 12:00 PM"
                         left={props => <List.Icon {...props} icon="pill" />}
                     />
                     <Divider />
                     <List.Item
+                        style={{marginLeft:"2%"}}
                         title="Take Nurofen"
                         description="Monday, 26th May, 11:00 AM"
                         left={props => <List.Icon {...props} icon="pill" />}
                     />
                     <Divider />
                     <List.Item
+                        style={{marginLeft:"2%"}}
                         title="Take Aspirin"
                         description="Tuesday, 27th May, 3:00 PM"
                         left={props => <List.Icon {...props} icon="pill" />}
                     />
                     <Divider />
                     <List.Item
+                        style={{marginLeft:"2%"}}
                         title="Take Calcivid"
                         description="Wednesday, 28th May, 8:00 AM"
                         left={props => <List.Icon {...props} icon="pill" />}
                     />
                     <Divider />
                 </List.Section>
+                </ScrollView>
             </View>
         </View>
     </MainLayout >
