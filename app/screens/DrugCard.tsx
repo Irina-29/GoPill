804a4ecdc06d64de003bd1f8442289731d5377ed
Irina-29 <irina.barbu229@gmail.com>
2022-04-19 12:12:23 +0300

    update

--- app/screens/DrugCard.tsx
+++ app/screens/DrugCard.tsx
@@ -1,23 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { Avatar, Button, Card, Paragraph, Title } from "react-native-paper";
-import { View, Image, Text, ScrollView } from "react-native";
+import { View, Image, Text, ScrollView, StyleProp, ViewStyle, StyleSheet } from "react-native";
 
-const LeftContent = (props: any) => <Avatar.Icon {...props} icon="folder" />
+const LeftContent = () => <Image style={{width: 90, height: 60}} source={require('./assets/drugs/advil.jpg')} />
 
 export interface Drug {
     id: number
     name: string
     quantity: number
     price: number
+    image: File
 }
+
+const styles = StyleSheet.create({
+    container: {
+        margin: "4%"
+    },
+});
 
 const DrugCard = ({ drug }: {drug: Drug}) => {
     return (
-        <Card key={drug.id}>
-            <Card.Title title={drug.name} left={LeftContent} />
-            <Card.Content>
-                <Paragraph>Card content</Paragraph>
-            </Card.Content>
+        <Card key={drug.id} style={styles.container}>
+            <Card.Title subtitleStyle={{marginLeft: '18%'}} titleStyle={{marginLeft: '18%'}} title={drug.name} subtitle={`${drug.quantity}x pills`} left={LeftContent}/>      
         </Card>
     )
 }
--- app/screens/DrugsList.tsx
+++ app/screens/DrugsList.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MainLayout from "./Layout";
-import { Button, Appbar } from "react-native-paper";
+import { Button, Appbar, Searchbar } from "react-native-paper";
 import { View, Image, Text, ScrollView } from "react-native";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import DrugCard, { Drug } from "./DrugCard";
@@ -19,13 +19,29 @@ const DrugsScreen = ({ navigation }: any) => {
             id: 1,
             name: "Advil",
             quantity: 24,
-            price: 13.49
+            price: 13.49,
+            image: require('./assets/drugs/advil.jpg')
         },
         {
             id: 2,
+            name: "Nurofen",
+            quantity: 24,
+            price: 25.30,
+            image: require('./assets/drugs/nurofen.jpg')
+        },
+        {
+            id: 3,
             name: "Paracetamol",
             quantity: 20,
-            price: 3.00
+            price: 3.00,
+            image: require('./assets/drugs/paracetamol.jpg')
+        },
+        {
+            id: 4,
+            name: "Vitamin C",
+            quantity: 30,
+            price: 22.50,
+            image: require('./assets/drugs/vitamin-c.png')
         }
     ]
 
